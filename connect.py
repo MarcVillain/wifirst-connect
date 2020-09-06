8ccd5ee9eed9045e01cf3f4bbb5711cb880a0b6f
Marc Villain <marc.villain@epita.fr>
2020-09-07 01:11:06 +0200

    refacto(connect): invert error-handling logic by raising it on top and cleanup typos

--- connect.py
+++ connect.py
@@ -38,112 +38,111 @@ def connect(username, password):
         logger.info("Already logged in.")
         return True
 
-    elif web_page.status_code == 302:
-
-        # Redirected to the Wifirst login site
-        try:
-            wifirst_page = requests.get("https://selfcare.wifirst.net/sessions/new")
-        except:
-            logger.error("Unable to reach Wifirst selfcare site.")
-            return False
-
-        if wifirst_page.status_code == 200:
-
-            # Get the form token value
-            # The token is the only value with 46 characters
-            try:
-                token_regex = re.compile('value="([^"]{30,})"')
-                token = token_regex.search(str(wifirst_page.content)).group(1)
-            except:
-                logger.error("Unable to retrieve the login token.")
-                return False
-
-            logger.info(f"Login token: {token}.")
-
-            # A session is used to keep the cookies through the process
-            session = requests.Session()
-            post_data = {
-                "login": username,
-                "password": password,
-                "authenticity_token": token,
-                "utf8": "&#x2713",
-            }
-
-            # Send the authentication form, with the credentials
-            try:
-                wifirst_page = session.post(
-                    "https://selfcare.wifirst.net/sessions", data=post_data, allow_redirects=False
-                )
-            except:
-                logger.error("Unable to reach Wifirst selfcare site with a POST request.")
-                return False
-
-            # If the authenticatino is ok, a redirection will take place
-            if wifirst_page.status_code == 302:
-
-                try:
-                    wifirst_page = session.get("https://connect.wifirst.net/?perform=true")
-                except:
-                    logger.error("Unable to reach Wifirst redirected site.")
-                    return False
-
-                # The returned page content has an automatically filled form, that will submit on loading
-                # On a Browser, as the content has an 'onLoad' function, it will automatically send the
-                # form with the credentials
-
-                # Wifirst login form
-                try:
-                    form_regex = re.compile('action="(https://[a-zA-Z0-9:\./]+)"')
-                    username_regex = re.compile('name="username"\s+type="hidden"\s+value="(w/[0-9]+@wifirst.net)"')
-                    password_regex = re.compile('name="password"\s+type="hidden"\s+value="([a-zA-Z0-9]+)"')
-
-                    form_val = form_regex.search(str(wifirst_page.content)).group(1)
-                    username_val = username_regex.search(str(wifirst_page.content)).group(1)
-                    password_val = password_regex.search(str(wifirst_page.content)).group(1)
-                except:
-                    logger.error("Unable to retrieve the Wifirst Credentials.")
-                    return False
-
-                logger.info(f"Wifirst Usename='{username_val}' LoginPage = '{form_val}'.")
-
-                # Now send the second authentication form and credentials
-                post_data = {
-                    "username": username_val,
-                    "password": password_val,
-                    "qos_class": "",
-                    "success_url": TEST_URL,
-                    "error_url": "https://connect.wifirst.net/login_error",
-                }
-
-                try:
-                    wifirst_page = session.post(form_val, data=post_data, allow_redirects=True)
-                except:
-                    logger.error("Unable to reach Wifirst login form.")
-                    return False
-
-                if wifirst_page.status_code == 200:
-                    if wifirst_page.url == TEST_URL:
-                        logger.info("Authentication sucessfull.")
-                        return True
-                    else:
-                        logger.error(f"Authentication failed, redirected to {wifirst_page.url}.")
-                else:
-                    logger.error("Final authentication failed with error {str(wifirst_page.status_code)}.")
-                    return False
-
-            # If the self care POST returns 200, it means that the Wifirst authentiation failed
-            elif wifirst_page.status_code == 200:
-                logger.error("Wrong credentials.")
-                return False
-            else:
-                logger.error(f"Wifirst loging form returned {str(wifirst_page.status_code)}")
-                return False
-        else:
-            logger.error("Unable to reach Wifirst redirected site.")
-            return False
-    else:
+    if web_page.status_code != 302:
         logger.error(f"Unexpected HTTP result for {TEST_URL}.")
         return False
+
+    # Redirected to the Wifirst login page
+    try:
+        wifirst_page = requests.get("https://selfcare.wifirst.net/sessions/new")
+    except:
+        logger.error("Unable to reach Wifirst selfcare site.")
+        return False
+
+    if wifirst_page.status_code != 200:
+        logger.error("Unable to reach Wifirst redirected site.")
+        return False
+
+    # Get the form token value
+    # The token is the only value with 46 characters
+    try:
+        token_regex = re.compile('value="([^"]{30,})"')
+        token = token_regex.search(str(wifirst_page.content)).group(1)
+    except:
+        logger.error("Unable to retrieve the login token.")
+        return False
+
+    logger.info(f"Login token: {token}.")
+
+    # A session is used to keep the cookies through the process
+    session = requests.Session()
+    post_data = {
+        "login": username,
+        "password": password,
+        "authenticity_token": token,
+        "utf8": "&#x2713",
+    }
+
+    # Send the authentication form, with the credentials
+    try:
+        wifirst_page = session.post(
+            "https://selfcare.wifirst.net/sessions", data=post_data, allow_redirects=False
+        )
+    except:
+        logger.error("Unable to send the credentials through the authentication form.")
+        return False
+
+    # If the self care POST returns 200, it means that the Wifirst authentiation failed
+    if wifirst_page.status_code == 200:
+        logger.error("Wrong credentials.")
+        return False
+
+    # If the authentication is ok, a redirection should take place
+    if wifirst_page.status_code != 302:
+        logger.error(f"Wifirst logging form returned {str(wifirst_page.status_code)}")
+        return False
+
+    try:
+        wifirst_page = session.get("https://connect.wifirst.net/?perform=true")
+    except:
+        logger.error("Unable to reach Wifirst redirected site.")
+        return False
+
+    # The returned page content has an automatically filled form, that will submit on loading.
+    # On a Browser, as the content has an 'onLoad' function, it will automatically send the
+    # form with the credentials.
+    # Here, we don't have a browser so the form will have to be sent manually.
+
+    # Wifirst login form
+    try:
+        action_regex = re.compile('action="(https://[a-zA-Z0-9:\./]+)"')
+        username_regex = re.compile('name="username"\s+type="hidden"\s+value="(w/[0-9]+@wifirst.net)"')
+        password_regex = re.compile('name="password"\s+type="hidden"\s+value="([a-zA-Z0-9]+)"')
+
+        action_val = action_regex.search(str(wifirst_page.content)).group(1)
+        username_val = username_regex.search(str(wifirst_page.content)).group(1)
+        password_val = password_regex.search(str(wifirst_page.content)).group(1)
+    except:
+        logger.error("Unable to retrieve the Wifirst credentials.")
+        return False
+
+    logger.info(f"Wifirst Usename='{username_val}' Password='******' LoginPage='{action_val}'.")
+
+    # Now send the second authentication form and credentials
+    post_data = {
+        "username": username_val,
+        "password": password_val,
+        "qos_class": "",
+        "success_url": TEST_URL,
+        "error_url": "https://connect.wifirst.net/login_error",
+    }
+
+    try:
+        wifirst_page = session.post(action_val, data=post_data, allow_redirects=True)
+    except:
+        logger.error("Unable to reach Wifirst login form.")
+        return False
+
+    if wifirst_page.status_code != 200:
+        logger.error(f"Final authentication failed with error {str(wifirst_page.status_code)}.")
+        return False
+
+    if wifirst_page.url != TEST_URL:
+        logger.error(f"Authentication failed, redirected to {wifirst_page.url}.")
+        return False
+
+    logger.info("Authentication successful.")
+    return True
 
 
 def setup_logging(verbose, silent, output):
@@ -182,6 +181,8 @@ def main(args):
             time.sleep(LOGIN_RETRY_INTERVAL)
 
 
+# File execution handling
+# -----------------------
 def parse_command_line(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action="store_true", help="Set logging level to DEBUG.")
